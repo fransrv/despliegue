0a4b04105675900ba043993d262853aec0f2b194
Fran <palaciech@gmail.com>
2017-02-06 20:44:42 +0100

    Titulo cambiado

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Despliegue de Aplicaciones LAMP</title>
+<title>Despliegue de Aplicaciones GitHub</title>
 </head>
 <body>
-<h1>Despliegue de aplicaciones LAMP</h1>
+<h1>Despliegue de aplicaciones GitHub</h1>
 <?php
 phpinfo();
 ?>
